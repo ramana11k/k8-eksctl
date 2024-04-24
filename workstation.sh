06e6d3513423125ca209a5af5934a5fdf37b07f0
ramana11k <ramana11k@gmail.com>
2024-04-24 07:49:44 +0530

    kubernates

--- workstation.sh
+++ workstation.sh
@@ -76,4 +76,11 @@ sudo git clone https://github.com/ahmetb/kubectx /opt/kubectx
 sudo ln -s /opt/kubectx/kubens /usr/local/bin/kubens
 VALIDATE $? "kubens installation"
 
+#install helm
+
+curl -fsSL -o get_helm.sh https://raw.githubusercontent.com/helm/helm/main/scripts/get-helm-3
+chmod 700 get_helm.sh
+./get_helm.sh
+VALIDATE $? "Helm installation"
+
 echo -e "$R Logout and login again $N"
